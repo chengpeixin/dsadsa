fd8058539e1fcbcb119cc81f3e855e4c6a958cc2
xpc <xinpeicheng@yeah.net>
2018-06-30 17:17:05 +0800

    123

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = new koa();
 const serve = require("koa-static");
 
-const port = 8001;
+const port = 8008;
 app
   .use(serve(__dirname + "/", {
     extensions: ['html']
